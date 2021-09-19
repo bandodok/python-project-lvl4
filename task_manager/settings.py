4072c709665eaad18971b8543493da0948366013
Dmitrij Svetov <bandodok@gmail.com>
2021-09-19 13:58:10 +0300

    added settings for heroku

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 
 
 # Application definition
