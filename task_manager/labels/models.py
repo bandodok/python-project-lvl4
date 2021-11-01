179d5e6550f5bbb5c9c442909106f456bd8facc6
Dmitrij Svetov <bandodok@gmail.com>
2021-11-01 18:57:35 +0300

    created labels model

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Label(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
