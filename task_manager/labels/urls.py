a5bb8b8f21eb3cbe6ac3e82c37f2f5a0f5d2c7d8
Dmitrij Svetov <bandodok@gmail.com>
2021-11-01 18:57:20 +0300

    updated labels urls.py

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from task_manager.statuses import views
+from task_manager.labels import views
 
 
 urlpatterns = [
     path('', views.Index.as_view(), name='labels'),
-    path('create/', views.Create.as_view(), name='labels_create'),
-    path('<int:pk>/update/', views.Update.as_view(), name='labels_update'),
-    path('<int:pk>/delete/', views.Delete.as_view(), name='labels_delete'),
+    path('create/', views.Create.as_view(), name='label_create'),
+    path('<int:pk>/update/', views.Update.as_view(), name='label_update'),
+    path('<int:pk>/delete/', views.Delete.as_view(), name='label_delete'),
 ]
