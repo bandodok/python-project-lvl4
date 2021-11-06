bb63245b42e138ba1c366dd0971cbb03aa5312a5
Dmitrij Svetov <bandodok@gmail.com>
2021-11-06 17:27:12 +0300

    injected full name return to User __str__ field

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+def get_full_name(self):
+    return f'{self.first_name} {self.last_name}'
+
+
+User.add_to_class("__str__", get_full_name)
+
+
 class Profile(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
     firstname = models.CharField(max_length=100)
