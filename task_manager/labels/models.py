b96281090a70b368578aa15f6fdb6e50be8de8e8
Dmitrij Svetov <bandodok@gmail.com>
2021-11-06 15:07:17 +0300

    updated localization

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
 
 class Label(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(_('Name'), max_length=100)
 
     def __str__(self):
         return self.name
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -2,16 +2,17 @@ from django.db import models
 from task_manager.statuses.models import Status
 from task_manager.labels.models import Label
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
 
 
 class Task(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE, related_name='status')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
-    executor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='executor')
-    labels = models.ManyToManyField(Label)
+    name = models.CharField(_('Name'), max_length=100)
+    description = models.CharField(_('Description'), max_length=500)
+    status = models.ForeignKey(Status, verbose_name=_('Status'), on_delete=models.CASCADE, related_name='status')
+    author = models.ForeignKey(User, verbose_name=_('Author'), on_delete=models.CASCADE, related_name='author')
+    executor = models.ForeignKey(User, verbose_name=_('Executor'), on_delete=models.CASCADE, related_name='executor')
+    labels = models.ManyToManyField(Label, verbose_name=_('Labels'))
 
     def __str__(self):
         return self.name
