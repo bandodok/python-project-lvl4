f30c15164b64df431ad69920c1ea822ab835e45a
Dmitrij Svetov <bandodok@gmail.com>
2021-11-06 13:55:29 +0300

    updated localization

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -140,7 +140,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'ru'
 
 LANGUAGES = [
     ('ru', _('Russian')),
